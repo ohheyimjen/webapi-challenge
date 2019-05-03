265e17d782c66e13d4d6bb552ee067937ec5cab8
ohheyimjen <jendority@gmail.com>
2019-05-03 09:06:22 -0700

    added actionsRouter.js and projectsRouter.js with needed imports/exports

---
+++ data/actions/actionsRouter.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const Actions = require('../helpers/actionModel');
+
+const router = express.Router();
+
+router.use(express.json());
+
+
+module.exports = router;
---
+++ data/projects/projectsRouter.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const Projects = require('../helpers/projectModel');
+
+const router = express.Router();
+
+router.use(express.json());
+
+
+
+module.exports = router;
