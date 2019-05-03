5b150c7111dab19ef1de198b1855021b6eb55582
ohheyimjen <jendority@gmail.com>
2019-05-03 10:42:58 -0700

    setup APIs in server.js

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
 
+const projectsRouter = require('./data/projects/projectsRouter');
+const actionsRouter = require('./data/actions/actionsRouter');
+
 const server = express();
 
 server.use(express.json());
+server.use('/projects', projectsRouter);
+server.use('/actions', actionsRouter);
+
+server.get('/', (req, res) => {
+  res.send(`<h2>Arrrre yooouuuu Readddyyyyy for the Sprint Chaaaaallllennnngggge???`);
+});
 
 module.exports = server;
